1257463847dc54e53ee664767d099e31f2dbfbcf
wesley <wesleysantos20012001@gmail.com>
2023-03-15 17:11:07 -0300

    feat [enviroment]: implementation of enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, MoviesController],
   providers: [AppService, MoviesService],
 })
